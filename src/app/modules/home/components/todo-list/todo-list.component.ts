aee5722d7984114467524b8e3d17f6c5588263a1
henrique <henriquebd2000.hs@gmail.com>
2023-01-10 11:48:40 -0300

    added functionality: sort item according to checked

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck} from '@angular/core';
+import { first, last } from 'rxjs';
 import { TaskList } from '../../model/task-list';
 
 @Component({
@@ -6,17 +7,18 @@ import { TaskList } from '../../model/task-list';
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent implements OnInit{
+export class TodoListComponent implements DoCheck{
 
-  
+
   public taskList: Array<TaskList> = [
   ]
 
   constructor() {}
 
-  ngOnInit(): void {
+  ngDoCheck(): void {
+    this.taskList.sort((first, last) => Number(first.checked) - Number(last.checked)) // sort(): um método que ordena um array, onde quando o retorno do método é -1 o array é ordenado de forma crescente e quando é 1 é ordenado de forma crescente. Nesse caso o atributo checked é booleano e está sendo convertido para número (ou seja, 0 ou 1), então o método está ordenando pelo valor do checked 
   }
-  
+
   public deleteItemTaskList(event: number){
     this.taskList.splice(event, 1)
   }
@@ -30,5 +32,15 @@ export class TodoListComponent implements OnInit{
 
   public setEmitTaskList(event: string){
     this.taskList.push({task: event, checked: false})
+  }
+
+  public validationInput(event: string, index: number){
+    if(!event.length){
+      const confirm = window.confirm('Task está vazia, deseja DELETA-LA?')
+
+      if (confirm) {
+        this.deleteItemTaskList(index)
+      }
+    }
   }
 }
